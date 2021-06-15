d3bd0b8dff76ad790c5f95e49736e043ef7499a2
Paul Hernández <mpaul.hernandez@gmail.com>
2021-06-14 23:11:20 -0500

    Update Game class initialization
    Use dependency injection to pass in overridable input and input streams.
    Abstract game logic into private mehtods

--- lib/rangman/game.rb
+++ lib/rangman/game.rb
@@ -5,36 +5,57 @@ module Rangman
   class Game
     attr_reader :lives, :word, :board
 
-    def initialize
-      @lives = Lives.new
-      @word  = Words.sample
-      @board = Board.new(word)
+    def initialize(input = $stdin, output = $stdout)
+      @input  = input
+      @output = output
+
+      @lives  = Lives.new
+      @word   = Words.sample
+      @board  = Board.new(word)
     end
 
     def start
-      puts "Welcome to Rangman!\n\n"
+      output_game_start
 
       while lives.remaining? && board.incomplete?
-        puts lives.state
-        puts board.state
+        output_lives_state
+        output_board_state
 
-        make_a_guess
+        input_and_process_guess
       end
 
-      puts board.incomplete? ? 'You loose!' : 'You won!'
+      output_game_result
     end
 
-    def make_a_guess
-      if board.reveal_character(guess)
-        # nothing else to do
-      else
-        lives.decrease
-      end
+    private
+
+    def output_game_start
+      @output.puts "Welcome to Rangman!\n\n"
     end
 
-    def guess
-      print "\nPlease guess a letter: "
-      gets.chomp
+    def output_lives_state
+      @output.puts lives.state
+    end
+
+    def output_board_state
+      @output.puts "Guess this word: #{board.state}"
+    end
+
+    def input_and_process_guess
+      guess = player_guess
+
+      lives.decrease unless board.reveal_character(guess)
+
+      guess
+    end
+
+    def player_guess
+      @output.puts "\nPlease guess a letter: "
+      @input.gets.chomp
+    end
+
+    def output_game_result
+      @output.puts board.incomplete? ? 'You loose!' : 'You won!'
     end
   end
 end
--- spec/rangman/game_spec.rb
+++ spec/rangman/game_spec.rb
@@ -3,11 +3,55 @@
 require 'rangman'
 
 RSpec.describe Rangman::Game do
-  subject(:game) { described_class.new }
+  subject(:game) { described_class.new(game_input, game_output) }
+
+  let(:game_input) { StringIO.new(format_guesses(player_guesses)) }
+  let(:game_output) { StringIO.new }
+  let(:player_guesses) { %w[d o g] }
+
+  let(:game_word) { 'dog' }
 
   describe '#start' do
-    it 'asks for a guess while the player has remaining lives or the board remains incomplete' do
-      expect(game.start)
+    before do
+      allow(Rangman::Words).to receive(:sample).and_return(game_word)
+    end
+
+    it 'outputs the game word' do
+      hidden_game_word = game_word.chars.map { '_' }
+
+      game.start
+
+      expect(game_output.string).to include("Guess this word: #{hidden_game_word.join(' ')}")
+    end
+
+    it 'asks the player for a guess' do
+      allow(game_input).to receive(:gets).and_return("x\n")
+
+      game.start
+
+      expect(game_output.string).to include('Please guess a letter: ')
+    end
+
+    context 'when the player loses all its lives before guessing the word' do
+      let(:player_guesses) { %w[f a i l u r e] }
+
+      it 'outputs a failing message' do
+        game.start
+
+        expect(game_output.string.lines(chomp: true).last).to eq('You loose!')
+      end
+    end
+
+    context 'when the player guesses the word' do
+      it 'outputs a winning message' do
+        game.start
+
+        expect(game_output.string.lines(chomp: true).last).to eq('You won!')
+      end
     end
   end
+end
+
+def format_guesses(guesses)
+  "#{guesses.join("\n")}\n"
 end
